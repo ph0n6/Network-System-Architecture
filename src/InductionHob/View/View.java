5ecbe8e03d0f28f66a00516ba58ac9d2b1e9234e
ph0n6 <ph0n6@hotmail.com>
2018-10-06 20:05:10 +0700

    Use interface for view/ Add usage for cooking mode

--- src/InductionHob/View/View.java
+++ src/InductionHob/View/View.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,9 +14,9 @@ import java.util.ResourceBundle;
 public class View implements Initializable, ViewInterface {
     /* View Button */
     @FXML
-    TextField tempDisplay;
+    Text tempDisplay;
     @FXML
-    TextField timerDisplay;
+    Text timerDisplay;
     @FXML
     Button decreaseTempButton;
     @FXML
@@ -31,29 +32,38 @@ public class View implements Initializable, ViewInterface {
 
     public void initialize(URL location, ResourceBundle resources) {
         // Add option to cooking mode
-        cookingMode.getItems().addAll("Fry", "Steam", "Boil");
-        cookingMode.setPromptText("Change Cooking mode");
+        cookingMode.getItems().addAll("Fry", "Steam", "Boil", "Custom");
+        //cookingMode.setPromptText("Use Cooking preset");
     }
-
+    @Override
     public void increaseTemp(){
         System.out.println("Increase Temp");
     }
-
+    @Override
     public void decreaseTemp(){
         System.out.println("Decrease Temp");
     }
-
+    @Override
+    public void setTempDisplay() {System.out.println("Set Temp display");}
+    @Override
+    public void setTimerDisplay() {System.out.println("Set Timer display");}
+    @Override
     public void powerHob(){
         System.out.println("Power off");
     }
-
+    @Override
     public void lockHob(){
         System.out.println("Lock");
     }
-
+    @Override
     public void setTimer(){
         System.out.println("Set Timer");
     }
-
-
+    @Override
+    public void chooseCookingMode(){
+        cookingMode.setOnAction(e -> setCookingMode());
+    }
+    public void setCookingMode(){
+        System.out.println("Set cooking mode to " + cookingMode.getValue());
+    }
 }
--- src/InductionHob/View/ViewInterface.java
+++ src/InductionHob/View/ViewInterface.java
@@ -1,4 +1,14 @@
 package InductionHob.View;
 
 public interface ViewInterface {
+    public void increaseTemp();
+    public void decreaseTemp();
+    public void setTempDisplay();
+    public void setTimerDisplay();
+    public void powerHob();
+    public void lockHob();
+    public void setTimer();
+    public void chooseCookingMode();
 }
+
+
